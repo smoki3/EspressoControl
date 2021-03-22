5842e492b842d566b5bf2e5dd30170af081bedf2
smoki3 <kevinbayer@macbook-pro-von-kevin.fritz.box>
2021-03-22 22:37:52 +0100

    Increase max. Boiler Temp

--- sloeber.ino.cpp
+++ sloeber.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2021-03-02 19:59:43
+//This file has been generated on 2021-03-22 16:39:39
 
 #include "Arduino.h"
 #include "Arduino.h"
